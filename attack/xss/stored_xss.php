1f1987eecac9d39325bc3d7ed89035ebcc573e0f
JIHO <ljhymca@naver.com>
2021-05-27 14:47:52 +0900

    특수문자 치환 규칙 추가

--- attack/xss/stored_xss.php
+++ attack/xss/stored_xss.php
@@ -12,6 +12,7 @@
 </html>
 <?php
     $file_path = './stored.txt';
+    $patterns = "/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i";//특수문자목록
     $fp = fopen($file_path, 'r');
     readfile($file_path);
     if(isset($_POST['text']))
@@ -23,6 +24,20 @@
             exit;
         }
         */
+        /*
+        if(preg_match('/</',$_POST['text']))
+        {
+            echo "XSS FAILED";
+            exit;
+        }
+        */
+        if(preg_match_all($patterns,$_POST['text']))
+        {
+            $string=preg_replace($patterns,"",$_POST['text']);//특수문자 치환
+            echo $string;
+            echo "<br> you can't use script";
+            exit;
+        }
         $fp = fopen($file_path, 'w');
         flock($fp, LOCK_EX);
         fwrite($fp, $_POST['text']  .chr(13).chr(10), strlen($_POST['text']));
